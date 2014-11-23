19ecefb95fd7c12c842c21ee3bef78be6feaf6b9
KamilBest <kamil.besst@gmail.com>
2014-11-23 20:41:58 +0100

    +chalkboard in features (needs small fix)

--- chalk.js
+++ chalk.js
@@ -1,35 +1,37 @@
 $(document).ready(chalkboard);
 function chalkboard(){
-	$('#chalkboard').remove();
-	$('.chalk').remove();
-	$('body').prepend('<canvas id="chalkboard"></canvas>');
-	$('body').prepend('<div class="chalk"></div>');
+    $('#chalkboard').remove();
+    $('.chalk').remove();
+    //board position
+    $('.board').prepend('<canvas id="chalkboard"></canvas>');
+    $('.board').prepend('<div class="chalk"></div>');
 
-	var canvas = document.getElementById("chalkboard");
-	$('#chalkboard').css('width',$(window).width());
-	$('#chalkboard').css('height',$(window).height());
-	canvas.width = $(window).width();
-	canvas.height = $(window).height();
+    var canvas = document.getElementById("chalkboard");
+    $('#chalkboard').css('width',$(".board").width());
+    $('#chalkboard').css('height',$(".board").height());
+    canvas.width = $(".board").width();
+	canvas.height = $(".board").height();
 
-	var ctx = canvas.getContext("2d");
+    var ctx = canvas.getContext("2d");
 
-	var width = canvas.width;
-	var height = canvas.height;
-	var mouseX = 0;
-	var mouseY = 0;
-	var mouseD = false;
-	var xLast = 0;
-	var yLast = 0;
-	var brushDiameter = 7;
+    var width = canvas.width;
+    var height = canvas.height;
+    var mouseX = 0;
+    var mouseY = 0;
+    var mouseD = false;
+    var xLast = 0;
+    var yLast = 0;
+    var brushDiameter = 8;
 
-	$('#chalkboard').css('cursor','none');
-	document.onselectstart = function(){ return false; };
-	ctx.fillStyle = 'rgba(255,255,255,0.5)';
-	ctx.strokeStyle = 'rgba(255,255,255,0.5)';
+
+    $('#chalkboard').css('cursor','none');
+    document.onselectstart = function(){ return false; };
+    ctx.fillStyle = 'rgba(255,255,255,0.5)';
+    ctx.strokeStyle = 'rgba(255,255,255,0.5)';
     ctx.lineWidth = brushDiameter;
-	ctx.lineCap = 'round';
+    ctx.lineCap = 'round';
 
-	document.addEventListener('touchmove', function(evt) {
+    document.addEventListener('touchmove', function(evt) {
         var touch = evt.touches[0];
         mouseX = touch.pageX;
         mouseY = touch.pageY;
@@ -57,60 +59,58 @@ function chalkboard(){
         mouseD = false;
     }, false);
     $('#chalkboard').css('cursor','none');
-	ctx.fillStyle = 'rgba(255,255,255,0.5)';
-	ctx.strokeStyle = 'rgba(255,255,255,0.5)';
+    ctx.fillStyle = 'rgba(255,255,255,0.5)';
+    ctx.strokeStyle = 'rgba(255,255,255,0.5)';
     ctx.lineWidth = brushDiameter;
-	ctx.lineCap = 'round';
+    ctx.lineCap = 'round';
 
-	$(document).mousemove(function(evt){
-		mouseX = evt.pageX;
-		mouseY = evt.pageY;
-		if(mouseY<height && mouseX<width){
-			$('.chalk').css('left',(mouseX-0.5*brushDiameter)+'px');
-			$('.chalk').css('top',(mouseY-0.5*brushDiameter)+'px');
-			if(mouseD){
-				draw(mouseX,mouseY);
-				}
-		}else{
-			$('.chalk').css('top',height-10);
-			}
-		});
-	$(document).mousedown(function(evt){
-		mouseD = true;
-		xLast = mouseX;
-		yLast = mouseY;
-		draw(mouseX+1,mouseY+1);
-		});
-	$(document).mouseup(function(evt){
-		mouseD = false;
-		});
+    $(document).mousemove(function(evt){
+        mouseX = evt.pageX;
+        mouseY = evt.pageY;
+        if(mouseY<height && mouseX<width){
+            $('.chalk').css('left',(mouseX-0.5*brushDiameter)+'px');
+            $('.chalk').css('top',(mouseY-0.5*brushDiameter)+'px');
+            if(mouseD){
+                draw(mouseX,mouseY);
+            }
+        }else{
+            $('.chalk').css('top',height-10);
+        }
+    });
+    $(document).mousedown(function(evt){
+        mouseD = true;
+        xLast = mouseX;
+        yLast = mouseY;
+        draw(mouseX+1,mouseY+1);
+    });
+    $(document).mouseup(function(evt){
+        mouseD = false;
+    });
 
-	function draw(x,y){
-		ctx.strokeStyle = 'rgba(255,255,255,'+(0.4+Math.random()*0.2)+')';
-		ctx.beginPath();
-  		ctx.moveTo(xLast, yLast);
-  		ctx.lineTo(x, y);
-  		ctx.stroke();
+    function draw(x,y){
+        ctx.strokeStyle = 'rgba(255,255,255,'+(0.8+Math.random()*0.2)+')';
+        ctx.beginPath();
+        ctx.moveTo(xLast, yLast);
+        ctx.lineTo(x, y);
+        ctx.stroke();
 
-  		// Chalk Effect
-		var length = Math.round(Math.sqrt(Math.pow(x-xLast,2)+Math.pow(y-yLast,2))/(5/brushDiameter));
-		var xUnit = (x-xLast)/length;
-		var yUnit = (y-yLast)/length;
-		for(var i=0; i<length; i++ ){
-			var xCurrent = xLast+(i*xUnit);
-			var yCurrent = yLast+(i*yUnit);
-			var xRandom = xCurrent+(Math.random()-0.5)*brushDiameter*1.2;
-			var yRandom = yCurrent+(Math.random()-0.5)*brushDiameter*1.2;
-    		ctx.clearRect( xRandom, yRandom, Math.random()*2+2, Math.random()+1);
-			}
+        // Chalk Effect
+        var length = Math.round(Math.sqrt(Math.pow(x-xLast,2)+Math.pow(y-yLast,2))/(5/brushDiameter));
+        var xUnit = (x-xLast)/length;
+        var yUnit = (y-yLast)/length;
+        for(var i=0; i<length; i++ ){
+            var xCurrent = xLast+(i*xUnit);
+            var yCurrent = yLast+(i*yUnit);
+            var xRandom = xCurrent+(Math.random()-0.5)*brushDiameter*1.2;
+            var yRandom = yCurrent+(Math.random()-0.5)*brushDiameter*1.2;
+            ctx.clearRect( xRandom, yRandom, Math.random()*2+2, Math.random()+1);
+        }
+        xLast = x;
+        yLast = y;
+    }
 
+    $(window).resize(function(){
+        chalkboard();
+    });
 
-		xLast = x;
-		yLast = y;
-		}
-
-	$(window).resize(function(){
-			chalkboard();
-		});
-
-	}
+}
