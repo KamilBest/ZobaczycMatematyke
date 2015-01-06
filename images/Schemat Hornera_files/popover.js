e61578fec58ece9fe0dba5c7c2b6c16824dd6803
KamilBest <kamil.besst@gmail.com>
2015-01-06 21:25:32 +0100

    popovers, horner's method table, examples

---
+++ images/Schemat Hornera_files/popover.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($){
+
+$('.popover-markup > .trigger').popover({
+    html : true,
+    title: function() {
+      return $(this).parent().find('.head').html();
+    },
+    content: function() {
+      return $(this).parent().find('.content').html();
+    },
+    container: 'body',
+    placement: 'right'
+});
+    });
