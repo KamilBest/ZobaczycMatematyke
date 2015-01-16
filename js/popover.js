8948e339a592d04eee89fe92fc94e0d60b2a38ec
KamilBest <kamil.besst@gmail.com>
2015-01-16 18:59:50 +0100

    amendments, working at new sections

--- js/popover.js
+++ js/popover.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
     $('.trigger').popover();
 });
+$('html').on('click', function(e) {
+  if (typeof $(e.target).data('original-title') == 'undefined') {
+    $('[data-original-title]').popover('hide');
+  }
+});
